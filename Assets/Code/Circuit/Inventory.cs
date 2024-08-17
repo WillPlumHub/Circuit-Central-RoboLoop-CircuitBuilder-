178c5f8f8ec9676c7015ec85599138002da2cfe7
WillPlumHub <80426184+WillPlumHub@users.noreply.github.com>
2024-08-16 18:38:40 -0700

    Added more tile functionality & inventory. Currently dealing with recognizing tile being replaced. It's always the same as the tile it's replaced with

--- Assets/Code/Circuit/Inventory.cs
+++ Assets/Code/Circuit/Inventory.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Tilemaps;
 
 [System.Serializable]
@@ -9,33 +11,46 @@ public struct InventorySlots {
     public int slotID;
     public TileBase tile;
     public int amount;
+    
+    public InventorySlots(string name, int slotID, TileBase tile, int amount) {
+        this.name = name;
+        this.slotID = slotID;
+        this.tile = tile;
+        this.amount = amount;
+    }
 }
+
 public class Inventory : MonoBehaviour {
 
-    public TileBase Tile;
     public InventorySlots vert;
     public InventorySlots hori;
     public InventorySlots corner;
     
-    List<InventorySlots> inventory = new List<InventorySlots>();
+    public List<InventorySlots> inventory = new List<InventorySlots>();
     
     private void Start() {
+
+        vert.name = "Straight_Vertical";
+        //vert.slotID = 1;
+        vert.tile = AssetDatabase.LoadAssetAtPath<TileBase>("Assets/Prefabs/Tiles/Circoncept_2.asset");
+        vert.amount = 2;
+        hori.name = "Straight_Horizontal";
+        //hori.slotID = 2;
+        hori.tile = AssetDatabase.LoadAssetAtPath<TileBase>("Assets/Prefabs/Tiles/Circoncept_0.asset");
+        hori.amount = 2;
+        corner.name = "Corner";
+        //corner.slotID = 3;
+        corner.tile = AssetDatabase.LoadAssetAtPath<TileBase>("Assets/Prefabs/Tiles/Circoncept_3.asset");
+        corner.amount = 2;
+
         inventory.Add(vert);
         inventory.Add(hori);
         inventory.Add(corner);
 
-        vert.name = "Straight Vertical";
-        vert.slotID = 1;
-        vert.tile = Tile;
-        vert.amount = 2;
-        hori.name = "Straight Horizontal";
-        hori.slotID = 2;
-        hori.tile = Tile;
-        hori.amount = 2;
-        corner.name = "Corner";
-        corner.slotID = 1;
-        corner.tile = Tile;
-        corner.amount = 2;
+        for (int i = 0; i < inventory.Count; i++) {
+            InventorySlots tempItem = inventory[i];
+            tempItem.slotID = i + 1;
+            inventory[i] = tempItem;
+        }
     }
-
 }
--- Assets/Code/Circuit/TilePlacer.cs
+++ Assets/Code/Circuit/TilePlacer.cs
@@ -5,29 +5,63 @@ using UnityEngine.Tilemaps;
 
 public class TilePlacer : MonoBehaviour {
 
-    public TileBase currTile;
+    public int inventory_Item = 1;
+    public InventorySlots currItem;
     public Vector2 clickPos; 
     public Vector2 clickGridPos;
     public Tilemap Tilemap;
 
     public Inventory inventory;
-    
+
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
             clickPos = Input.mousePosition;
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
             Vector3Int gridPos = Tilemap.WorldToCell(worldPos);
             
-            if (Tilemap.HasTile(gridPos)) {
-                Debug.Log(worldPos + " " + gridPos);
-                Tilemap.SetTile(gridPos, currTile);
-                //Tilemap.InsertCells(gridPos, new Vector3Int(1,1,1));
+            if (Tilemap.HasTile(gridPos) && currItem.amount > 0 &&
+                
+                Tilemap.GetTile(gridPos).name != inventory.inventory[inventory_Item].name) {
+
+                //Debug.Log(Tilemap.GetTile(gridPos).name != inventory.inventory[inventory_Item].name);
+                //Debug.Log("H" + Tilemap.GetTile(gridPos).name + "H     H" + inventory.inventory[inventory_Item].tile.name + "H");
+                    
+                InventorySlots tempItem = inventory.inventory[inventory_Item];
+                tempItem.amount--;
+                inventory.inventory[inventory_Item] = tempItem;
+                
+                Tilemap.SetTile(gridPos, inventory.inventory[inventory_Item].tile);
+                
             }
         }
+
+        //Keyboard shortcuts
+        if (Input.GetKey("1")) {
+            inventory_Item = 0;
+        }
+        if (Input.GetKey("2")) {
+            inventory_Item = 1;
+        }
+        if (Input.GetKey("3")) {
+            inventory_Item = 2;
+        }
+
+        InventorySelect();
     }
 
     public void InventorySelect() {
         //slect itemID = 1 from inventory for now, register user selection later
-        inventory.vert.
+        if (inventory_Item >= inventory.inventory.Count) {
+            Debug.Log("Fuck You");
+            inventory_Item--;
+        }
+        else if (inventory_Item < 0) {
+            Debug.Log("Fuck You");
+            inventory_Item++;
+        }
+        else {
+            currItem = inventory.inventory[inventory_Item];
+        }
+        
     }
 }
