2791ef08fe6955c91ee670368a04e56e794b84fd
WillPlumHub <80426184+WillPlumHub@users.noreply.github.com>
2024-08-16 21:55:53 -0700

    Still can't get the weapon's projectiles to work for some reason >:(

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -5,8 +5,37 @@ using UnityEngine;
 public class Weapon : MonoBehaviour {
 
     public bool firing = false;
+    
+    
+    
+    
+    
+
+    public GameObject projectile;  // The projectile to spawn
+    public Transform spawnPoint;         // The point where the projectile is spawned
+    public float firingRate = 1f;      // Time in seconds between each shot
+    public float activeDuration = 5f;      // Total duration to fire projectiles
+
 
     public void OnTriggerEnter2D(Collider2D collision) {
-        firing = true;
+        StartCoroutine(shoot());
+    }
+
+    private void Update() {
+        if (firing == true) {
+            
+            
+            
+        }
+    }
+
+    IEnumerator shoot() {
+        float elapsedTime = 0f;
+        while (elapsedTime < activeDuration) {
+            Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(firingRate);
+            elapsedTime += firingRate;
+        }
+
     }
 }
