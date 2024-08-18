0352cc8c4271a214e9bc598475b96ec542b0e39a
Devin Sturgeon <dwsturge@gmail.com>
2024-08-18 10:21:32 -0400

    active reload bar updates
    progresses with a random mulitplier.
    Still need to slow it down
    Add event listener.

--- Assets/Code/Circuit/PowerBar.cs
+++ Assets/Code/Circuit/PowerBar.cs
@@ -2,25 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 public class PowerBar : MonoBehaviour
 {
 
-    public float PowerBarMultiplier = 5f;
-    public bool PowerBarRoutineRunning = false;
+    public float powerBarChargeSpeed = .01F;
+    public bool powerBarRoutineRunning = false;
+    public bool powerBarRunningUp = true;
+    public float currentSliderValue;
 
-    [SerializeField] private Slider slider;
+    public Slider powerBarSlider;
+
+    private float powerBarMin;
+    private float powerBarMax;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSliderValue = powerBarSlider.value;
+        powerBarMin = powerBarSlider.minValue;
+        powerBarMax = powerBarSlider.maxValue;
+
+        powerBarChargeSpeed = UnityEngine.Random.Range(.1F, 1F);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        currentSliderValue = powerBarSlider.value;
+        powerBarSlider.value += powerBarChargeSpeed;
         
+        if(currentSliderValue == powerBarMax)
+        {
+            powerBarSlider.value = 0;
+            powerBarChargeSpeed = UnityEngine.Random.Range(.1F, 1F);
+        }
+
+        Debug.Log(currentSliderValue);
+
+        if(currentSliderValue < powerBarMax)
+        {
+            powerBarSlider.value += powerBarChargeSpeed;
+        }
     }
 }
